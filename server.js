41198545885b474618552b73bd58357d3569df28
fran14cruz <fran14.ich@gmail.com>
2024-11-11 11:10:06 -0500

    update profile and image endpoints for database

--- server.js
+++ server.js
@@ -70,6 +70,31 @@ app.post('/register', (req, res) => {
     .catch(err => res.status(400).json('unable to register'));
 })
 
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  db.select('*').from('users').where({id})
+    .then(user => { // if not found, the database returns an empty array
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'));
+})
+
+app.put('/image', (req, res) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(error => res.status(400).json('unable to get entries'))
+})
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 });
