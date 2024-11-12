e407c03606c6596b578bf83b6c82ac811a3d1141
fran14cruz <fran14.ich@gmail.com>
2024-11-12 18:05:17 -0500

    Migrate clarifai to server

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,50 @@
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key 105b4021b51a4fc1a9e02031329bddc9"); // my key in my account in clarifai
+
+const handleApiCall = (req, res) => {
+  stub.PostModelOutputs(
+    {
+        // This is the model ID of a publicly available General model. You may use any other public or custom model ID.
+        model_id: "face-detection",
+        inputs: [{data: {image: {url: req.body.input}}}]
+    },
+    metadata,
+    (err, response) => {
+        if (err) {
+            console.log("Error: " + err);
+            return;
+        }
+
+        if (response.status.code !== 10000) {
+            console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+            return;
+        }
+
+        console.log("Predicted concepts, with confidence values:")
+        for (const c of response.outputs[0].data.concepts) {
+            console.log(c.name + ": " + c.value);
+        }
+        res.json(response);
+    }
+  );
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
     .then(entries => {
-      res.json(entries[0]);
+      res.json(entries[0].entries);
     })
     .catch(error => res.status(400).json('unable to get entries'))
 }
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 }
